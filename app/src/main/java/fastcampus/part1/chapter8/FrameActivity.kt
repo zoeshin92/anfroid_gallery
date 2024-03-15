1e5f09bd46168dd7ea1bfffdf0fd034d6ac4ae36
신혜규 <zoeshin92@gmail.com>
2024-03-15 11:24:45 +0900

    init commit

--- app/src/main/java/fastcampus/part1/chapter8/FrameActivity.kt
+++ app/src/main/java/fastcampus/part1/chapter8/FrameActivity.kt
@@ -1,10 +1,7 @@
 package fastcampus.part1.chapter8
 
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import com.google.android.material.tabs.TabLayoutMediator
 import fastcampus.part1.chapter8.databinding.ActivityFrameBinding
 
 class FrameActivity : AppCompatActivity() {
@@ -13,35 +10,5 @@ class FrameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFrameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.toolbar.apply {
-            title = "나만의 앨범"
-            setSupportActionBar(this)
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        val images = (intent.getStringArrayExtra("images") ?: emptyArray())
-            .map { uriString ->  FrameItem(Uri.parse(uriString)) }
-        val frameAdapter = FrameAdapter(images)
-
-        binding.viewPager.adapter = frameAdapter
-
-        TabLayoutMediator(
-            binding.tabLayout,
-            binding.viewPager,
-        ) {
-            tab, position ->
-            binding.viewPager.currentItem = tab.position
-        }.attach()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            android.R.id.home -> {
-                finish()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
