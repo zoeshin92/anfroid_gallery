1ab417b3f912a89ade9edfb06f7df0549ed9b419
신해윤 <zoeshin92@gmail.com>
2024-03-15 14:52:11 +0900

    need to continue

--- app/src/main/java/fastcampus/part1/chapter8/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/chapter8/MainActivity.kt
@@ -1,12 +1,121 @@
 package fastcampus.part1.chapter8
 
-
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import fastcampus.part1.chapter8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val imageLoadLauncher =
+        registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+            updateImages(uriList)
+        }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageAdapter: ImageAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    private fun initRecyclerView() {
+        imageAdapter = ImageAdapter(object: ImageAdapter.ItemClickListener{
+            override fun onLoadMoreClick() {
+                checkPermission()
+            }
+        })
+        binding.imageRecyclerView.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(context, 2)
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+
+            shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                showPermissionInfoDialog()
+            }
+
+            else -> {
+                requestReadExternalStorage()
+            }
+        }
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해서, 외부 저장소 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestReadExternalStorage()
+            }
+        }.show()
+    }
+
+    private fun loadImage() {
+        imageLoadLauncher.launch("image/*")
+    }
+
+    private fun requestReadExternalStorage() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun updateImages(uriList: List<Uri>) {
+        val images = uriList.map { ImageItems.Image(it) }
+        val updatedImages = imageAdapter.currentList.toMutableList().apply { addAll(images) }
+        imageAdapter.submitList(updatedImages)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                val resultCode = grantResults.firstOrNull() ?: PackageManager.PERMISSION_GRANTED
+                if (resultCode == PackageManager.PERMISSION_GRANTED) {
+                    loadImage()
+                }
+            }
+        }
+    }
+
+
+    companion object {
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
     }
 }
